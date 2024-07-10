c9c055ca34fdf160c7aff0a48da2880e97b9a9c3
Ashish Gurram <ashishchinnu0@gmail.com>
2024-07-10 20:15:02 +0530

    Testing GH pages

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from "./App";
 import App5 from "./App5";
 import Login from "./Login";
 
+const sqrfunction = (n) =>{
+    return n*n;
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App flag={false} />);
+// root.render(<App flag={false} />);
 // root.render(<App5 name="Cathy"/>);
-// root.render(<Login/>)
+root.render(<App14 result={{'John': 40 , 'Cathy' : 50}}/>)
